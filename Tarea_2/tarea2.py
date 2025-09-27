740aff2e4cf61284bd06a9a7e0efa55ca0e11eae
Bcontrerascoloma <bcontrerascoloma@gmail.com>
2025-09-27 23:27:51 +0000

    Termux

--- Tarea_2/tarea2.py
+++ Tarea_2/tarea2.py
@@ -6,7 +6,7 @@ p1 = pasajero("11.111.111-1", "Juan", "Elite Plus")
 p2 = pasajero("22.222.222-2", "Ana", "Platinum")
 p3 = pasajero("33.333.333-3", "Luis", "Gold")
 p4 = pasajero("44.444.444-4", "Carla", "Flyer")
-
+L = lista(p1, lista(p2, lista(p3, lista(p4, listaVacia))))
 #esPasajeroValido: pasajero -> bool
 #devuelve True si es un pasajero valido y False si no lo es
 #ejemplo: esPasajeroValido(pasajero("22.142.429-7","Benjamin", "Gold"))
@@ -16,10 +16,10 @@ def esPasajeroValido(p):
 assert esPasajeroValido(pasajero("22.142.429-7","Benjamin","Gold"))
 assert not esPasajeroValido(pasajero("12.345.678-9","Antonia",23))
 
+
 #listaCategoria(L,categoria):: lista(pasajero), str -> lista(pasajero)
 #devuelve una lista de pasajeros que pertenecen a la categoria dada.
-#ejemplo
-
+#ejemplo listarCategoria(L, "Gold") -> lista(valor=pasajero(rut='33.333.333-3', nombre='Juan', categoria='Elite Plus'),listaVacia) 
 def listarCategoria(L, c):
     assert esLista(L)
     assert esPasajeroValido(cabeza(L)) or esListaVacia(cabeza(L))
@@ -30,11 +30,12 @@ def listarCategoria(L, c):
         return lista(cabeza(L), listarCategoria(cola(L), c))
     else:
         return listarCategoria(cola(L), c)
-
-# lista enlazada de pasajeros
-L = lista(p1, lista(p2, lista(p3, lista(p4, listaVacia))))
 assert listarCategoria(L,"No Fidelizado") == listaVacia
+assert listarCategoria(L, "Gold") == lista(valor=pasajero(rut='33.333.333-3', nombre='Luis', categoria='Gold'),siguiente=None)
 
+#Concatenar(L1,L2):: lista(pasajero), lista(pasajero) -> lista(pasajero)
+#esta funcion lo que hace es concatenar la lista L2 al final de L1, si L1 es vacia retorna L2 si L2 es vacia, retorna L1
+        #concatenar(L,L)== lista(valor=pasajero(rut='11.111.111-1', nombre='Juan', categoria='Elite Plus'), siguiente=lista(valor=pasajero(rut='22.222.222-2', nombre='Ana', categoria='Platinum'), siguiente=lista(valor=pasajero(rut='33.333.333-3', nombre='Luis', categoria='Gold'), siguiente=lista(valor=pasajero(rut='44.444.444-4', nombre='Carla', categoria='Flyer'), siguiente=lista(valor=pasajero(rut='11.111.111-1', nombre='Juan', categoria='Elite Plus'), siguiente=lista(valor=pasajero(rut='22.222.222-2', nombre='Ana', categoria='Platinum'), siguiente=lista(valor=pasajero(rut='33.333.333-3', nombre='Luis', categoria='Gold'), siguiente=lista(valor=pasajero(rut='44.444.444-4', nombre='Carla', categoria='Flyer'), siguiente=None))))))))`
 def concatenar(L1,L2):
     assert esLista(L1)
     assert esPasajeroValido(cabeza(L1)) or esListaVacia(cabeza(L1))
@@ -44,13 +45,16 @@ def concatenar(L1,L2):
         return L2
     else:
         return lista(cabeza(L1),concatenar(cola(L1),L2))
-#print(concatenar(L,L))
+assert assert concatenar(L,L) == lista(valor=pasajero(rut='11.111.111-1', nombre='Juan', categoria='Elite Plus'), siguiente=lista(valor=pasajero(rut='22.222.222-2', nombre='Ana', categoria='Platinum'), siguiente=lista(valor=pasajero(rut='33.333.333-3', nombre='Luis', categoria='Gold'), siguiente=lista(valor=pasajero(rut='44.444.444-4', nombre='Carla', categoria='Flyer'), siguiente=lista(valor=pasajero(rut='11.111.111-1', nombre='Juan', categoria='Elite Plus'), siguiente=lista(valor=pasajero(rut='22.222.222-2', nombre='Ana', categoria='Platinum'), siguiente=lista(valor=pasajero(rut='33.333.333-3', nombre='Luis', categoria='Gold'), siguiente=lista(valor=pasajero(rut='44.444.444-4', nombre='Carla', categoria='Flyer'), siguiente=None)))))))) 
+
 #ordenarPasajeros(L):: lista(pasajero) ->lista(pasajero)
 #devuelve una lista con los pasajeros ordenados según su prioridad
+#ej: ordenarPasajeros(L) -> lista(valor=pasajero(rut='11.111.111-1', nombre='Juan', categoria='Elite Plus'), siguiente=lista(valor=pasajero(rut='22.222.222-2', nombre='Ana', categoria='Platinum'), siguiente=lista(valor=pasajero(rut='33.333.333-3', nombre='Luis', categoria='Gold'), siguiente=lista(valor=pasajero(rut='44.444.444-4', nombre='Carla', categoria='Flyer'), siguiente=lista(valor=pasajero(rut='44.444.444-4', nombre='Carla', categoria='Flyer'), siguiente=None)))))
 def ordenarPasajeros(L):
     assert esLista(L)
-    assert esPasajeroValido(cabeza(L)) or esListaVacia(cabeza(L))    
-   # resultado = lista(listarCategoria(L,"Elite Plus"),lista(listarCategoria(L,"Diamond")),lista(listarCategoria(L,"Platinum")),lista(listarCategoria(L,"Gold")),lista(listarCategoria(L,"Flyer")),lista(listarCategoria(L,"No Fidelizado"),listaVacia))
+    assert esPasajeroValido(cabeza(L)) or esListaVacia(cabeza(L))
     resultado = concatenar(listarCategoria(L,"Elite Plus"),concatenar(listarCategoria(L,"Diamond"),concatenar(listarCategoria(L,"Platinum"),concatenar(listarCategoria(L,"Gold"),concatenar(listarCategoria(L,"Flyer"),concatenar(listarCategoria(L,"Flyer"),listaVacia))))))
-    return resultador
-print(ordenarPasajeros(L))
+    return resultado
+assert ordenarPasajeros(L) == lista(valor=pasajero(rut='11.111.111-1', nombre='Juan', categoria='Elite Plus'), siguiente=lista(valor=pasajero(rut='22.222.222-2', nombre='Ana', categoria='Platinum'), siguiente=lista(valor=pasajero(rut='33.333.333-3', nombre='Luis', categoria='Gold'), siguiente=lista(valor=pasajero(rut='44.444.444-4', nombre='Carla', categoria='Flyer'), siguiente=lista(valor=pasajero(rut='44.444.444-4', nombre='Carla', categoria='Flyer'), siguiente=None))))) 
+
+
