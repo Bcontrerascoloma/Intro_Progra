67cd411e09f0a282b484ee8550f2834b17237dd4
Bcontrerascoloma <bcontrerascoloma@gmail.com>
2025-11-11 15:47:11 -0300

    Auto-sync: 2025-11-11 15:47:11

---
+++ objects_and_class/cafetera.py
@@ -0,0 +1,10 @@
+#atributos
+#color (str), maxima(int), actual(int)
+class Cafetera:
+    
+    #Constructor
+    def __init__(self,c,m,a):
+        self.__color = c
+        self.__maxima = m
+        self.__actual = a
+
